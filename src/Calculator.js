1b0e3fee2fdb98b950edd539bf2ed3bd462fa559
soyeong <mb535622@sookmyung.ac.kr>
2025-10-20 00:12:08 +0900

    feat(Calculator): calculate sum of numbers

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,10 +1,9 @@
 class Calculator {
     static sum(input, delimiter) {
         const regex = new RegExp(`[${delimiter.join('')}]`);
-
-        // 구분자를 기준으로 숫자를 분리
         const numbers = input.split(regex).map(Number);
 
+        return numbers.reduce((acc, curr) => acc + curr, 0);
     }
 }
 
